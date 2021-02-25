ad86a9450d322baa5fc9907717e7f9df4b7f8141
Alven Diaz <modulus@modulus-osx.local>
2021-02-25 11:27:36 -0800

    Changes env url

--- knexfile.ts
+++ knexfile.ts
@@ -3,14 +3,7 @@ import env from "./server/env";
 module.exports = {
   production: {
     client: "postgresql",
-    connection: {
-      host: env.DB_HOST,
-      database: env.DB_NAME,
-      user: env.DB_USER,
-      port: env.DB_PORT,
-      password: env.DB_PASSWORD,
-      ssl: env.DB_SSL,
-    },
+    connection: env.DATABASE_URL,
     migrations: {
       tableName: "knex_migrations",
       directory: "server/migrations"
